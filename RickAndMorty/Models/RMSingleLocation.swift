dc4ec764f2540326651dab18c742c250b58bfa2f
THL_Mac <thl_mac@THL-Macui-noteubug.local>
2024-09-24 22:40:46 +0900

    Add models

---
+++ RickAndMorty/Models/RMSingleLocation.swift
@@ -0,0 +1,13 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMorty
+//
+//  Created by THL_Mac on 9/24/24.
+//
+
+import Foundation
+
+struct RMSingleLocation: Codable {
+    let name: String
+    let url: String
+}
