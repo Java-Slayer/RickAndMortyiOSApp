772b89072077a171d626c92bb375e8d5e7a2fc22
THL_Mac <thl_mac@THL-Macui-noteubug.local>
2024-09-24 22:04:34 +0900

    Added docstrings and API related files

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by THL_Mac on 9/24/24.
+//
+
+import Foundation
+
+/// Object that represents a singlet API call
+final class RMRequest: Codable {
+    
+}
---
+++ RickAndMorty/APIClient/RMService.swift
@@ -0,0 +1,25 @@
+//
+//  RMService.swift
+//  RickAndMorty
+//
+//  Created by THL_Mac on 9/24/24.
+//
+
+import Foundation
+
+/// Primary API service object to get Rick and Morty data
+final class RMService {
+    /// Shared singleton instance
+    static let shared = RMService()
+    
+    /// Privatized contructor
+    private init() {}
+    
+    /// Send Rick and Morty API Call
+    /// - Parameters:
+    ///   - request: Request instance
+    ///   - completion: Callback with data or error
+    public func execute(_ request: RMRequest, completion: @escaping () -> Void) {
+        
+    }
+}
